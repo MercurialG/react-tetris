55fdb59fc46630e0e88146588b7b44018b148254
George Rykunov <43174725+MercurialG@users.noreply.github.com>
2022-10-26 22:00:00 -0500

    Board and Stats
    Board scaffold and Stats are added

--- src/components/Tetris/Tetris.jsx
+++ src/components/Tetris/Tetris.jsx
@@ -1,13 +1,17 @@
 import "./Tetris.css";
 import React from "react";
-import Board from "../Board";
-import useBoard from "../../hooks/useBoard"
+import Board from "../Board/Board";
+import useBoard from "../../hooks/useBoard";
+import {useGameStats} from "../../hooks/useGameStats";
+import GameStats  from "./../GameStats/GameStats";
 
 const Tetris = ({ rows, columns, setGameOver }) => {
+  const [gameStats, addLinesCleared] = useGameStats();
+  const [board, setBoard] = useBoard({ rows, columns });
   return (
     <div className="Tetris">
-      <Board />
-      <p>Tetris</p>
+      <Board board={board} />
+      <GameStats gameStats={gameStats} />
     </div>
   );
 };
